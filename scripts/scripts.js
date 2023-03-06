f8e58cfcbd0f0038f1516f596afcbc7c9ae944c7
Trang Trieu <79191593+doctortrang@users.noreply.github.com>
2023-03-06 11:04:34 +0700

    updated

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -63,14 +63,50 @@ let accessAnimalSection = document.getElementById("animal")
 let accessSideDrawer = document.getElementById("side-drawer")
 
 
-accessAnimalSection.addEventListener('mousemove', displaySideBar)
-accessSideDrawer.addEventListener('mouseleave', undodisplaySideBar)
+accessAnimalSection.addEventListener('mousemove', displaySideDrawer)
+accessSideDrawer.addEventListener('mouseleave', undodisplaySideDrawer)
 //console.log(accessAnimalSection)
-function displaySideBar() {
+function displaySideDrawer() {
     let event = document.getElementById("side-drawer")
     event.style.display = "block"
+    let event1 = document.getElementById("side-bar")
+    event1.style.display = "none"
 }
-function undodisplaySideBar() {
+function undodisplaySideDrawer() {
     let event = document.getElementById("side-drawer")
     event.style.display = "none"
+}
+
+let accessMenuButton = document.getElementById("menu-btn")
+let accessSideBar = document.getElementById("side-bar")
+
+accessMenuButton.addEventListener('mousemove', displaySideBar)
+accessSideBar.addEventListener('mouseleave', undodisplaySideBar)
+
+function displaySideBar() {
+    let event = document.getElementById("side-bar")
+    event.style.display = "block"
+    let event1 = document.getElementById("side-drawer")
+    event1.style.display = "none"
+}
+
+function undodisplaySideBar() {
+    let event = document.getElementById("side-bar")
+    event.style.display = "none"
+}
+let accessMainPage = document.getElementById("main-page")
+let accessPlants = document.getElementById("plants")
+let accessBlogs = document.getElementById("blogs")
+let accessSupport = document.getElementById("support")
+
+accessMainPage.addEventListener('mousemove', nodisplayIrrelevant)
+accessPlants.addEventListener('mousemove', nodisplayIrrelevant)
+accessBlogs.addEventListener('mousemove', nodisplayIrrelevant)
+accessSupport.addEventListener('mousemove', nodisplayIrrelevant)
+
+function nodisplayIrrelevant() {
+    let event = document.getElementById("side-bar")
+    event.style.display = "none"
+    let event1 = document.getElementById("side-drawer")
+    event1.style.display = "none"
 }
--- scripts/types.js
+++ scripts/types.js
@@ -1,14 +1,35 @@
 let accessAnimalSection = document.getElementById("animal")
 let accessSideDrawer = document.getElementById("side-drawer")
 
-accessAnimalSection.addEventListener('mousemove', displaySideBar)
-accessSideDrawer.addEventListener('mouseleave', undodisplaySideBar)
+
+accessAnimalSection.addEventListener('mousemove', displaySideDrawer)
+accessSideDrawer.addEventListener('mouseleave', undodisplaySideDrawer)
 //console.log(accessAnimalSection)
-function displaySideBar() {
+function displaySideDrawer() {
     let event = document.getElementById("side-drawer")
     event.style.display = "block"
+    let event1 = document.getElementById("side-bar")
+    event1.style.display = "none"
 }
-function undodisplaySideBar() {
+function undodisplaySideDrawer() {
     let event = document.getElementById("side-drawer")
+    event.style.display = "none"
+}
+
+let accessMenuButton = document.getElementById("menu-btn")
+let accessSideBar = document.getElementById("side-bar")
+
+accessMenuButton.addEventListener('mousemove', displaySideBar)
+accessSideBar.addEventListener('mouseleave', undodisplaySideBar)
+
+function displaySideBar() {
+    let event = document.getElementById("side-bar")
+    event.style.display = "block"
+    let event1 = document.getElementById("side-drawer")
+    event1.style.display = "none"
+}
+
+function undodisplaySideBar() {
+    let event = document.getElementById("side-bar")
     event.style.display = "none"
 }
