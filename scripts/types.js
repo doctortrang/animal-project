a4f9571edebb3ecd69995d406ad599d5e9d9785b
Trang Trieu <79191593+doctortrang@users.noreply.github.com>
2023-03-06 13:30:34 +0700

    update types.js

--- scripts/types.js
+++ scripts/types.js
@@ -33,3 +33,19 @@ function undodisplaySideBar() {
     let event = document.getElementById("side-bar")
     event.style.display = "none"
 }
+let accessMainPage = document.getElementById("main-page")
+let accessPlants = document.getElementById("plants")
+let accessBlogs = document.getElementById("blogs")
+let accessSupport = document.getElementById("support")
+
+accessMainPage.addEventListener('mousemove', nodisplayIrrelevant)
+accessPlants.addEventListener('mousemove', nodisplayIrrelevant)
+accessBlogs.addEventListener('mousemove', nodisplayIrrelevant)
+accessSupport.addEventListener('mousemove', nodisplayIrrelevant)
+
+function nodisplayIrrelevant() {
+    let event = document.getElementById("side-bar")
+    event.style.display = "none"
+    let event1 = document.getElementById("side-drawer")
+    event1.style.display = "none"
+}
